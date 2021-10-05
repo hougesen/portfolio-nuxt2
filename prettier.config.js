a781325e848540154022570ecbdf9cebc3dc4e87
Mads Hougesen <madshougesen@gmail.com>
2021-10-05 23:34:37 +0200

    chore: updated prettier config

---
+++ prettier.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    singleQuote: true,
+    jsxSingleQuote: true,
+    printWidth: 120,
+    jsxBracketSameLine: true,
+    trailingComma: 'all',
+    arrowParens: 'always',
+    endOfLine: 'lf',
+    tabWidth: 4,
+    semi: true,
+    quoteProps: 'as-needed',
+    bracketSpacing: true,
+    htmlWhitespaceSensitivity: 'css',
+    vueIndentScriptAndStyle: false,
+};
